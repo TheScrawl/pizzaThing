e5be51d087a54b2c61d80b03bdfcd3e2c38f6a73
TheScrawl <thescrawlgaming@gmail.com>
2018-03-28 22:53:19 +0000

    sfaf

--- pizzaThing.py
+++ pizzaThing.py
@@ -2,16 +2,24 @@ import os
 orderType = None
 def getOrderType(): 
 	global orderType
+	orderType = None
 	print('1: Delivery')
 	print('2: Pickup')
-	orderType = int(input())
-	if orderType == 1:
-		orderType = 'Delivery'
-	if orderType == 2:
-		orderType = 'Pickup'
-	else:
-		orderType == None
-		pass
+	
+	orderType = input()
+	try:
+		if int(orderType) == 1:
+			orderType = 'Delivery'
+		elif int(orderType) == 2:
+			orderType = 'Pickup'
+		else:
+			orderType = None
+			print('invalid option try again')
+			getOrderType()	
+	except ValueError:
+		orderType = None
+		print('invalid option try again')
+		getOrderType()
 getOrderType()
 
 class customer(object): 
@@ -36,52 +44,70 @@ gourmetList = ['unicorn', 'human (adult)', 'human (child)', 'food', 'dog biscuit
 gourmetOrderList = []
 regularOrderList = []
 
-def printPizza():
+def printPizza(pizzaType):
 	#Print pizza options
 	global orderList
-	print()
-	print('Standard Pizzas')
-	for index, val in enumerate(regularList):
-		print(str(index + 1) + ': ' + str(val))
-	print()
-	print('Gourmet Pizzas')
-	for index, val in enumerate(gourmetList):
-		print(str(index + 1) + ': ' + str(val))
-printPizza()
+	if pizzaType == 'regular':
+		print()
+		print('Standard Pizzas')
+		for index, val in enumerate(regularList):
+			print(str(index + 1) + ': ' + str(val))
+	if pizzaType == 'gourmet':
+		print()
+		print('Gourmet Pizzas')
+		for index, val in enumerate(gourmetList):
+			print(str(index + 1) + ': ' + str(val))
 
 def printOrder():
 	print('Regular Pizzas:')
 	for i in regularList:
 		print(i +  ': ' + str(regularOrderList.count(i)))
+	print()
 	print('Gourmet Pizzas:')
 	for i in gourmetList:
 		print(i + ': ' + str(gourmetOrderList.count(i)))
 
 def getPizza():
 	#get gourmet orders
+	os.system('clear')
+	printPizza('regular')
+	printPizza('gourmet')
+	print()
 	gourmetBool = input('Gourmet (y/n)? ')	
 	if gourmetBool == 'y':
+		os.system('clear')
+		printPizza('gourmet')
 		print('type "n" to cancel')
 		while True:
 			pizzaInput = input('Pizza Num: ')
 			try:
 				gourmetOrderList.append(gourmetList[int(pizzaInput) - 1])
 				os.system('clear')
+				printPizza('gourmet')
+				print()
 				printOrder()			
 			except ValueError:
+				pass
+			except IndexError:
 				pass
 			if str(pizzaInput) == 'n':
 				break
 	#get regular orders
 	if gourmetBool == 'n':
+		os.system('clear')
+		printPizza('regular')
 		print('type "n" to cancel')
 		while True:
 			pizzaInput = input('Pizza Num: ')
 			try:
-				regularOrderList.append(regularListList[int(pizzaInput) - 1])
+				regularOrderList.append(regularList[int(pizzaInput) - 1])
 				os.system('clear')
+				printPizza('regular')
+				print()
 				printOrder()
 			except ValueError:
+				pass
+			except IndexError:
 				pass
 			if str(pizzaInput) == 'n':
 				break
